7a316dd7a7add907595200d6c918efd659feadc2
Victor Widell <victor.widell@divid.se>
2026-09-24 17:31:20 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-    return <p>Hello World</p>;
-}
+import cartridges from "../public/cartridges.json";
+// import type { Cartridge } from "./Cartridge";
 
-export default App;
+export function App() {
+	return (
+		<>
+			<input list="cartridges" placeholder="Cartridge Serial" />
+			<datalist id="cartridges">
+				{cartridges.map((cartridge) => (
+					<option value={cartridge.code}>
+						{cartridge.titles.map((title) => (
+							<p>{title}</p>
+						))}
+					</option>
+				))}
+			</datalist>
+		</>
+	);
+}
