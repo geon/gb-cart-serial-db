84bd1bc0e3d00c6cf74c811f87f7791f41c4ee2b
Victor Widell <victor.widell@divid.se>
2026-09-24 21:44:55 +0200

    wip

--- node-src/importers/import-de.ts
+++ node-src/importers/import-de.ts
@@ -1,6 +1,8 @@
 import { readFileSync } from "fs";
 import type { Cartridge } from "../../src/Cartridge.js";
 
+const regionSuffixPattern = new RegExp(`, [A-Z]{2,3}(/[A-Z]{2,3})*$`);
+
 export function importDe(): readonly Cartridge[] {
 	const inputPath = "assets/de.txt";
 	const cartridges = readFileSync(inputPath, { encoding: "utf-8" })
@@ -9,7 +11,9 @@ export function importDe(): readonly Cartridge[] {
 		.map((line): Cartridge => {
 			const splitIndex = line.indexOf(" ");
 			const code = line.slice(0, splitIndex);
-			const title = line.slice(splitIndex + 2, -1);
+			const title = line
+				.slice(splitIndex + 2, -1)
+				.replace(regionSuffixPattern, "");
 			return {
 				code,
 				title,
