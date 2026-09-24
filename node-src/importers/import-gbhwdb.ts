ed41567b5e81e3e821c42d82e5236decab4b11b1
Victor Widell <victor.widell@divid.se>
2026-09-24 21:19:41 +0200

    wip

--- node-src/importers/import-gbhwdb.ts
+++ node-src/importers/import-gbhwdb.ts
@@ -4,6 +4,8 @@ import type { Cartridge } from "../../src/Cartridge.js";
 import type { CartridgesCsvRow } from "../CartridgesCsvRow.js";
 import { isDefined } from "../../src/is-defined.js";
 
+const namePattern = /[^(]+/;
+
 export function importGbhwdb(): readonly Cartridge[] {
 	const csvPath = "assets/gbhwdb-cartridges.csv";
 	const csvCartridges = parse<CartridgesCsvRow>(
@@ -11,14 +13,15 @@ export function importGbhwdb(): readonly Cartridge[] {
 	);
 
 	const cartridges = csvCartridges
-		.map((row): Cartridge | undefined =>
-			!row.code
+		.map((row): Cartridge | undefined => {
+			const title = row.game_name.match(namePattern)?.[0].trimEnd();
+			return !(row.code && title)
 				? undefined
 				: {
 						code: row.code,
-						title: row.game_name,
-					},
-		)
+						title,
+					};
+		})
 		.filter(isDefined);
 
 	return cartridges;
